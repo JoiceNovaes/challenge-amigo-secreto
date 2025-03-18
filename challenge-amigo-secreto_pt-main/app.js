fbb4b20ff00c6bdbbabc8b6285c90d2a5e01c223
Joice Novaes <joice_novaes@outlook.com>
2025-03-18 13:15:58 -0300

    projeto

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,44 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nomeAmigo = inputAmigo.value.trim();
+
+    if (nomeAmigo.length === 0) {
+        alert("Digite um nome!");
+        return;
+    }
+
+    amigos.push(nomeAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    atualizarLista();
+}
+
+function atualizarLista() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    amigos.forEach(amigo => {
+        let item = document.createElement("li");
+        item.textContent = amigo;
+        listaAmigos.appendChild(item);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Adicione um nome!");
+        return;
+    }
+
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultadoLista = document.getElementById("resultado");
+
+    resultadoLista.innerHTML = ""; 
+
+    let item = document.createElement("li");
+    item.textContent = sorteado ;
+
+    resultadoLista.appendChild(item);
+}
